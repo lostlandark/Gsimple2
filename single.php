81bf71d4d30e9216a498238c7f9b946dd5190e91
ggice <309683593@qq.com>
2014-02-25 21:14:24 +0800

    add share button

--- single.php
+++ single.php
@@ -39,6 +39,17 @@ get_header(); ?>
 				</div><!-- .entry -->
 				<footer class="entry-footer">
 					<?php edit_post_link( __( 'Edit Post', 'wpex' ), '<span class="edit-link clr">', '</span>' ); ?>
+                    <!-- JiaThis Button BEGIN -->
+                    <div class="jiathis_style">
+                        <span class="jiathis_txt">分享到：</span>
+                        <a class="jiathis_button_tools_1"></a>
+                        <a class="jiathis_button_tools_2"></a>
+                        <a class="jiathis_button_tools_3"></a>
+                        <a class="jiathis_button_tools_4"></a>
+                        <a href="http://www.jiathis.com/share" class="jiathis jiathis_txt jiathis_separator jtico jtico_jiathis" target="_blank">更多</a>
+                    </div>
+                    <script type="text/javascript" src="http://v3.jiathis.com/code_mini/jia.js" charset="utf-8"></script>
+                    <!-- JiaThis Button END -->
 				</footer><!-- .entry-footer -->
 			</article>
 			<?php
