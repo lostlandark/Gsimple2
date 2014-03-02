81b986a4d9f12b4898e06b3de9d0be74458be310
ggice <309683593@qq.com>
2014-03-02 21:31:07 +0800

    add sigle-page

---
+++ templates/single-portfolio.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package gsimple2
+ * Template Name: Portfolio
+ */
+get_header(); ?>
+        <div id="primary">
+            <div class="content" id="content-porfolio" role="main">
+
+            <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+                <div class="thumbnail-width"><?php the_post_thumbnail( 'single-post-thumbnail' ); ?></div>
+
+                <?php get_template_part( 'content', 'page' ); ?>
+
+                <?php comments_template(); ?>
+
+            <?php endwhile; // end of the loop. ?>
+
+            </div><!-- #content -->
+        </div><!-- #primary -->
+<?php get_footer(); ?>
